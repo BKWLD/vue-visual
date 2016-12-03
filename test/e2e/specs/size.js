3b72ca305f48282c0199ac578400cef0bcd54f64
Robert Reinhard <robert.reinhard@bkwld.com>
2016-12-02 16:06:36 -0800

    Flattening css classes
    Inherited styles were messing up visuals inside of visuals

--- test/e2e/specs/size.js
+++ test/e2e/specs/size.js
@@ -3,29 +3,29 @@ module.exports = {
   'it should support just width': function (browser) { browser
 		.url('http://localhost:8080/size/')
     .waitForElementVisible('#app', 1000)
-		.assert.cssProperty('.vv-has-width', 'width', '200px')
+		.assert.cssProperty('.test-width', 'width', '200px')
   },
 
   'it should support just height': function (browser) { browser
-		.assert.cssProperty('.vv-has-height', 'height', '100px')
+		.assert.cssProperty('.test-height', 'height', '100px')
   },
 
 	'it should support width and height': function (browser) { browser
-		.assert.cssProperty('.vv-has-width.vv-has-height', 'width', '200px')
-		.assert.cssProperty('.vv-has-width.vv-has-height', 'height', '100px')
+		.assert.cssProperty('.test-width-and-height', 'width', '200px')
+		.assert.cssProperty('.test-width-and-height', 'height', '100px')
   },
 
 	'it should support aspect ratios strings': function (browser) { browser
-		.assert.attributeContains('.aspect-ratio .vv-aspect-shim', 'style', `padding-top: ${9/16*100}%;`)
+		.assert.attributeContains('.test-ratio .vv-aspect-shim', 'style', `padding-top: ${9/16*100}%;`)
   },
 
   'it should support aspect fraction numbers': function (browser) { browser
-		.assert.attributeContains('.aspect-fraction .vv-aspect-shim', 'style', `padding-top: ${9/16*100}%;`)
+		.assert.attributeContains('.test-fraction .vv-aspect-shim', 'style', `padding-top: ${9/16*100}%;`)
   },
 
 	'it should support fill': function (browser) { browser
-		.assert.cssProperty('.vv-fill .vv-asset', 'height', '100px')
-		.assert.cssProperty('.vv-fill .vv-asset', 'width', '100px')
+		.assert.cssProperty('.test-fill .vv-asset', 'height', '100px')
+		.assert.cssProperty('.test-fill .vv-asset', 'width', '100px')
     .end()
   },
 
