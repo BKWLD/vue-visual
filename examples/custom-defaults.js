ea55df4ea46947bc9bf0212f2e6b0f9df4957d67
Robert Reinhard <robert.reinhard@bukwild.com>
2020-04-09 13:15:43 -0700

    Add an example of setting custom props

---
+++ examples/custom-defaults.js
@@ -0,0 +1,24 @@
+/**
+ * Example of using a functional component to set default props for all Visual
+ * instances in a project. 
+ */
+
+import Visual from 'vue-visual';
+
+Vue.component('visual', {
+  name: 'VueVisual',
+  functional: true,
+  render: function(create, {props, data}) {
+    return create(Visual, {
+      ...data,
+      props: {
+        
+        // Custom default props
+        lazyLoad: true,
+				autoplay: true,
+				
+        ...props
+      }
+    });
+  }
+});
